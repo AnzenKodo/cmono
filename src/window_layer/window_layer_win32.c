121d1fea2848fc42afa0ac8bd56a860e493e0dc7
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2025-09-17 18:33:19 +0530

    Added windows WindowProc function

--- src/window_layer/window_layer_win32.c
+++ src/window_layer/window_layer_win32.c
@@ -6,18 +6,120 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         case WM_DESTROY:
             PostQuitMessage(0);  // Quit the message loop
             return 0;
+LRESULT CALLBACK
+wl_w32_window_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    switch(uMsg)
+    {
+        default: {
+            result = DefWindowProcW(hwnd, uMsg, wParam, lParam);
+        }break;
+
+        case WM_ENTERSIZEMOVE: {
+        }break;
+        case WM_EXITSIZEMOVE: {
+        }break;
+
+        case WM_SIZE:
         case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            RECT rect;
-            GetClientRect(hwnd, &rect);
-            DrawText(hdc, TEXT("Hello, Win32!"), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-            EndPaint(hwnd, &ps);
-            return 0;
-        }
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }break;
+
+        case WM_CLOSE: {
+        }break;
+
+        case WM_LBUTTONUP:
+        case WM_MBUTTONUP:
+        case WM_RBUTTONUP: {
+        } // fallthrough;
+        case WM_LBUTTONDOWN:
+        case WM_MBUTTONDOWN:
+        case WM_RBUTTONDOWN: {
+            switch (uMsg)
+            {
+                case WM_LBUTTONUP: case WM_LBUTTONDOWN: {
+                }break;
+                case WM_MBUTTONUP: case WM_MBUTTONDOWN: {
+                }break;
+                case WM_RBUTTONUP: case WM_RBUTTONDOWN: {
+                }break;
+            }
+        }break;
+        case WM_MOUSEMOVE: {
+        }break;
+        case WM_MOUSEWHEEL: {
+        }break;
+
+        case WM_MOUSEHWHEEL: {
+        }break;
+
+        case WM_SYSKEYDOWN: case WM_SYSKEYUP: {
+        } // fallthrough;
+        case WM_KEYDOWN: case WM_KEYUP: {
+        }break;
+
+        case WM_SYSCHAR: {
+        }break;
+
+        case WM_CHAR: {
+        }break;
+
+        case WM_KILLFOCUS: {
+        }break;
+
+        case WM_SETCURSOR: {
+        }break;
+
+        case WM_DPICHANGED: {
+        }break;
+
+            //- rjf: [file drop]
+        case WM_DROPFILES: {
+        }break;
+
+            //- rjf: [custom border]
+        case WM_NCPAINT: {
+        }break;
+        case WM_DWMCOMPOSITIONCHANGED: {
+        }break;
+        case WM_WINDOWPOSCHANGED: {
+        }break;
+        case WM_NCUAHDRAWCAPTION:
+        case WM_NCUAHDRAWFRAME: {
+        }break;
+        case WM_SETICON:
+        case WM_SETTEXT: {
+        }break;
+
+        case WM_NCACTIVATE: {
+        }break;
+        case WM_NCCALCSIZE: {
+        }break;
+        case WM_NCHITTEST: {
+                if(window == 0 || window->custom_border == 0 || is_fullscreen)
+                {
+                    result = DefWindowProcW(hwnd, uMsg, wParam, lParam);
+                }
+                else
+                {
+                    result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+                    switch (result)
+                    {
+                        case HTNOWHERE:
+                        case HTRIGHT:
+                        case HTLEFT:
+                        case HTTOPLEFT:
+                        case HTTOPRIGHT:
+                        case HTBOTTOMRIGHT:
+                        case HTBOTTOM:
+                        case HTBOTTOMLEFT: {
+                        } break;
+                    }
+        }break;
     }
+    ProfEnd();
+    return result;
 }
 
 internal void
