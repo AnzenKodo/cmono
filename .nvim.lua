150b7dab82501afcb66e99c7bb008d8ea0d907e3
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2025-10-01 15:23:24 +0530

    Updated editors configs

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,5 @@
-vim.opt.makeprg="cc -ggdb build.c && ./a.out build-run mingw"
+if (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1) then
+    vim.opt.makeprg="a.exe build-run"
+else
+    vim.opt.makeprg="cc -ggdb build.c && ./a.out build-run mingw"
+end
