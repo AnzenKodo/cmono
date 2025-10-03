ea69e64dc355e4af3834fb5425cf22623e2848ad
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2025-10-03 09:58:24 +0530

    Made windows render layer files

---
+++ src/render/render_win32.h
@@ -0,0 +1,20 @@
+#ifndef RENDER_X11_H
+#define RENDER_X11_H
+
+// Types
+//=============================================================================
+
+typedef struct Render_W32_State Render_W32_State;
+struct Render_W32_State
+{
+};
+
+// Global Variables
+//=============================================================================
+
+global Render_W32_State render_W32_state = ZERO_STRUCT;
+
+// Functions
+//=============================================================================
+
+#endif // RENDER_X11_H
