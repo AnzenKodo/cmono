dd4e784f60eb7fd631256b57361dbd60a926ee92
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-02-16 02:49:21 +0530

    Added NeoVim debugger

--- .nvim.lua
+++ .nvim.lua
@@ -1,6 +1,27 @@
+local cc_command = ""
 if (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1) then
-    vim.opt.makeprg="setup_x64.bat && cl.exe build.c -nologo -Z7 -Fo:build\\ -Fe:build\\build.exe && build\\build.exe build-run --nocolor"
+    cc_command="setup_x64.bat && cl.exe build.c -nologo -Z7 -Fo:build\\ -Fe:build\\build.exe && build\\build.exe build-"
 else
-    vim.opt.makeprg="cc -ggdb build.c && ./a.out build-run --nocolor"
-    -- vim.opt.makeprg="cc -ggdb build.c"
+    cc_command="cc -ggdb build.c && ./a.out build-"
 end
+vim.opt.makeprg=cc_command .. "run --nocolor"
+
+-- Setup Termdebug ============================================================
+
+local termdebug_config = vim.g.termdebug_config or {}
+termdebug_config.command = {
+    "gdb", "-nx",
+    "-ex", "set breakpoint pending on",
+    "-ex", "set disassembly-flavor intel",
+    "-ex", "set confirm off",
+    -- "-ex", "tbreak entry_point",
+    "./build/cope_debug",
+}
+vim.g.termdebug_config = termdebug_config
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "TermdebugStartPre",
+    callback = function()
+        vim.cmd('cexpr system("' .. cc_command .. 'debugger --nocolor")')
+    end,
+})
