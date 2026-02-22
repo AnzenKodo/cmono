6b269f3d5944ce89e9dce7a39c38dbba9c90cff9
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-02-23 00:04:35 +0530

    Updated font module to make texture also better text rendering

--- src/font/font.c
+++ src/font/font.c
@@ -21,50 +21,70 @@ typedef int32_t  stbtt_int32;
 #define STBTT_memset       mem_set
 #include "./external/stb_truetype.h"
 
-internal Font font_load(Str8 name, float font_size, unsigned char atlas_width, unsigned char atlas_height, Arena *arena)
+internal Font font_load(Str8 name, size_t atlas_width, size_t atlas_height, Arena *arena)
 {
     Font font = ZERO_STRUCT;
     font.atlas_width = atlas_width;
     font.atlas_height = atlas_height;
     font.pixels = arena_push(arena, unsigned char, atlas_width * atlas_height);
+    font.size = FONT_SIZE_TTF_DEFAULT;
     // ak: Load font file
     Arena_Temp temp = arena_temp_begin(arena);
-    unsigned char *ttf_buffer = arena_push(temp.arena, unsigned char, MB(1));
-    Os_File font_file = os_file_open(name, OS_AccessFlag_Read);
-    os_file_read_full(font_file, ttf_buffer);
-    // ak: init font data
-    stbtt_fontinfo font_info;
-    stbtt_InitFont(&font_info, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer, 0));
-    stbtt_pack_context pc;
-    stbtt_PackBegin(&pc, font.pixels, atlas_width, atlas_height, 0, 1, NULL);
     {
-        stbtt_PackFontRange(&pc, ttf_buffer, 0, font_size, 32, 224, font.data);
+        unsigned char *ttf_buffer = arena_push(temp.arena, unsigned char, MB(1));
+        Os_File font_file = os_file_open(name, OS_AccessFlag_Read);
+        os_file_read_full(font_file, ttf_buffer);
+        // ak: init font data
+        stbtt_fontinfo font_info;
+        stbtt_InitFont(&font_info, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer, 0));
+        //~ ak: compute scale and metrics *before* packing
+        font.scale = stbtt_ScaleForPixelHeight(&font_info, 32.0f);
+        int ascent, descent, line_gap;
+        stbtt_GetFontVMetrics(&font_info, &ascent, &descent, &line_gap);
+        font.line_height = (ascent - descent + line_gap) * font.scale;
+        font.height = (ascent - descent) * font.scale;
+        stbtt_pack_context pc;
+        stbtt_PackBegin(&pc, font.pixels, atlas_width, atlas_height, 0, 1, NULL);
+        {
+            stbtt_PackFontRange(&pc, ttf_buffer, 0, font.size, 32, 224, font.data);
+        }
+        stbtt_PackEnd(&pc);
     }
-    stbtt_PackEnd(&pc);
     arena_temp_end(temp);
+    font.texture = render_tex2d_alloc(Render_Resource_Kind_Dynamic, Render_Tex2D_Format_R8, atlas_width, atlas_height, font.pixels, arena);
     return font;
 }
 
-// internal void font_write_letter(Font *font, char letter, float x, float y)
-// {
-//     stbtt_aligned_quad q;
-//     stbtt_GetPackedQuad(font->data, font->atlas_width, font->atlas_height, letter - 32, &x, &y, &q, true);
-// }
-//
-// typedef struct
-// {
-//    float x0,y0,s0,t0; // top-left
-//    float x1,y1,s1,t1; // bottom-right
-// } stbtt_aligned_quad;
-//
-// internal void font_write(Font *font, char letter, float x, float y)
-// {
-//     Vec4_F32 result[4] = ZERO_STRUCT;
-//     stbtt_aligned_quad q;
-//     stbtt_GetPackedQuad(font->data, font->atlas_width, font->atlas_height, letter - 32, &x, &y, &q, true);
-//     glTexCoord2f(q.s0, q.t0, q.x0, q.y0);
-//     glTexCoord2f(q.s1, q.t0, q.x1, q.y0);
-//     glTexCoord2f(q.s1, q.t1, q.x1, q.y1);
-//     glTexCoord2f(q.s0, q.t1, q.x0, q.y1);
-//     return (Vec4_F32)q;
-// }
+internal void font_unload(Font *font)
+{
+   render_tex2d_free(font->texture);
+}
+
+internal void font_quad_push(Font *font, Str8 text, Vec2_F32 pos, Vec4_F32 color, Render_Draw_List *list, Arena *arena)
+{
+    float x = pos.x;
+    float y = pos.y+(font->height/2);
+    for (char *c = (char *)text.cstr; *c; ++c)
+    {
+        if (*c < 32 || *c > 126)
+        {
+            if (*c == '\n')
+            {
+                y += font->line_height;
+                x = pos.x;
+            }
+        }
+        else
+        {
+            stbtt_aligned_quad q;
+            stbtt_GetPackedQuad(font->data, font->atlas_width, font->atlas_height, *c - 32, &x, &y, &q, true);
+            Render_Draw_Node *node = arena_push(arena, Render_Draw_Node, 1);
+            Render_Draw_Rect *rect = &node->param_rect;
+            rect->dst = (Vec4_F32){ q.x0, q.y0, q.x1-q.x0, q.y1-q.y0 };
+            rect->src = (Vec4_F32){ q.s0, q.t0, q.s1-q.s0, q.t1-q.t0 };
+            rect->texture = font->texture;
+            rect->color = color;
+            SLLQueuePush(list->first, list->last, node);
+        }
+    }
+}
--- src/font/font.h
+++ src/font/font.h
@@ -15,8 +15,25 @@ struct Font
 {
     stbtt_packedchar data[256];
     void *pixels;
-    unsigned char atlas_width;
-    unsigned char atlas_height;
+    size_t size;
+    float height;
+    float scale;
+    float line_height;
+    size_t atlas_width;
+    size_t atlas_height;
+    Render_Handle texture;
 };
+
+// Defines
+//=============================================================================
+
+#define FONT_SIZE_TTF_DEFAULT 32
+
+// Functions
+//=============================================================================
+
+internal Font font_load(Str8 name, size_t atlas_width, size_t atlas_height, Arena *arena);
+internal void font_unload(Font *font);
+internal void font_quad_push(Font *font, Str8 text, Vec2_F32 pos, Vec4_F32 color, Render_Draw_List *list, Arena *arena);
 
 #endif // FONT_H
