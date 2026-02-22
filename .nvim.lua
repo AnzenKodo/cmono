1060cee346626125c77d43e7578cd76f828144e3
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-02-22 23:56:06 +0530

    Updated NeoVim settings file

--- .nvim.lua
+++ .nvim.lua
@@ -15,13 +15,19 @@ termdebug_config.command = {
     "-ex", "set disassembly-flavor intel",
     "-ex", "set confirm off",
     -- "-ex", "tbreak entry_point",
-    "./build/cope_debug",
+    "./build/cope_debug"
 }
 vim.g.termdebug_config = termdebug_config
 
 vim.api.nvim_create_autocmd("User", {
-    pattern = "TermdebugStartPre",
+    pattern = "TermdebugStartPost",
     callback = function()
+        vim.keymap.set("n", "<leader>dr",  ":call TermDebugSendCommand('dr')<CR>", { desc = "[d]ebugger [r]un (modify)" })
         vim.cmd('cexpr system("' .. cc_command .. 'debugger --nocolor")')
+        vim.fn.TermDebugSendCommand('define dr')
+        vim.fn.TermDebugSendCommand('   shell cc build.c && ./a.out build-debugger')
+        vim.fn.TermDebugSendCommand('   file build/cope_debug')
+        vim.fn.TermDebugSendCommand('   run')
+        vim.fn.TermDebugSendCommand('end')
     end,
 })
