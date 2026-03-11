598915ab37c5fe4feda39177ab761bb4b03add76
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-03-12 03:54:29 +0530

    Updated Neovim config

--- .nvim.lua
+++ .nvim.lua
@@ -1,21 +1,40 @@
+-- TODO(ak): Add every build user command and debug also make helper functions
+-- for making them.
+
+-- ak: Build Initialization
+-- ============================================================================
+
+local build_dir = "build"
 local build_command = ""
 local cc_command = ""
 if (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1) then
-    build_command="setup_x64.bat && cl.exe build.c -nologo -Z7 -Fo:build\\ -Fe:build\\build.exe"
-    cc_command=build_command.." && build\\build.exe build-"
+    build_command = "setup_x64.bat && cl.exe build.c -nologo -Z7 -Fo:build\\ -Fe:"..build_dir.."\\build.exe"
+    cc_command = build_command.." && "..build_dir.."\\build.exe "
 else
-    build_command="cc -ggdb build.c"
-    cc_command=build_command.." && ./a.out build-"
+    build_command = "cc -ggdb build.c"
+    cc_command = build_command.." && ./a.out "
 end
-vim.opt.makeprg=cc_command .. "run --nocolor"
+
+-- ak: Build user commands
+-- ============================================================================
+
+-- NOTE(ak): run with `:make` below command
+vim.opt.makeprg = cc_command .. "build-run --nocolor"
 vim.api.nvim_create_user_command("Build",  function()
-    vim.opt.makeprg=build_command
+    vim.opt.makeprg = build_command
     vim.cmd('make')
-    vim.opt.makeprg=cc_command .. "run --nocolor"
+    vim.opt.makeprg = cc_command .. "build-run --nocolor"
+end, { desc = "Bootstrap build system"})
+vim.api.nvim_create_user_command("Meta",  function()
+    vim.opt.makeprg = cc_command .. "gen-meta --nocolor"
+    vim.cmd('make')
+    vim.opt.makeprg = cc_command .. "build-run --nocolor"
 end, { desc = "Bootstrap build system"})
 
--- Setup Termdebug ============================================================
+-- ak: Setup Termdebug
+-- ============================================================================
 
+local program_name_debug = build_dir.."/cope_debug"
 local termdebug_config = vim.g.termdebug_config or {}
 termdebug_config.command = {
     "gdb", "-nx",
@@ -23,7 +42,7 @@ termdebug_config.command = {
     "-ex", "set disassembly-flavor intel",
     "-ex", "set confirm off",
     "-ex", "set print pretty on",
-    "./build/cope_debug"
+    program_name_debug
 }
 vim.g.termdebug_config = termdebug_config
 
@@ -31,10 +50,10 @@ vim.api.nvim_create_autocmd("User", {
     pattern = "TermdebugStartPost",
     callback = function()
         vim.keymap.set("n", "<leader>dr",  ":call TermDebugSendCommand('dr')<CR>", { desc = "[d]ebugger [r]un (modify)" })
-        vim.cmd('cexpr system("' .. cc_command .. 'debugger --nocolor")')
+        vim.cmd('cexpr system("' .. cc_command .. 'build-debugger --nocolor")')
         vim.fn.TermDebugSendCommand('define dr')
-        vim.fn.TermDebugSendCommand('   shell cc build.c && ./a.out build-debugger')
-        vim.fn.TermDebugSendCommand('   file build/cope_debug')
+        vim.fn.TermDebugSendCommand('   shell '..cc_command.."build-debugger")
+        vim.fn.TermDebugSendCommand('   file '..program_name_debug)
         vim.fn.TermDebugSendCommand('   run')
         vim.fn.TermDebugSendCommand('end')
     end,
