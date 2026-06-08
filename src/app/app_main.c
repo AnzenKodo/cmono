4fd05877dbd087c98684d203382d31cb55533616
AnzenKodo <50282743+AnzenKodo@users.noreply.github.com>
2026-06-08 09:29:49 +0530

    app: update window and rendering setup for multi-window support

--- src/app/app_main.c
+++ src/app/app_main.c
@@ -1,8 +1,5 @@
-// TODO(ak): add multi-window support
 // TODO(ak): update event handling
-// TODO(ak): make flags state based
 // TODO(ak): make fmt printf's internal
-// TODO(ak): add freelist in render texture
 
 //- ak: headers
 #include "./app.h"
@@ -28,13 +25,14 @@ internal void base_main(void)
     // Program Init ===========================================================
     unsigned int width = 150;
     unsigned int height = 300;
-    wl_window_open(str8(APP_NAME), width, height);
-    wl_window_border_set(false);
-    wl_window_pos_set(wl_display_width_get()-width-30, 0);
-    render_init();
+    wl_init();
+    Wl_Window window = wl_window_open(str8(APP_NAME), width, height);
+    wl_window_border_set(window, false);
+    wl_window_pos_set(window, wl_display_width_get()-width-30, 0);
+    render_init(window);
     
     // Program Loop ===========================================================
-    while (!wl_should_window_close())
+    while (!wl_should_exit())
     {
         render_begin();
         wl_set_fps(60);
@@ -42,7 +40,7 @@ internal void base_main(void)
         if ((wl_is_key_pressed(Wl_Key_Esc)) ||
             wl_is_event_happen(Wl_EventType_WindowClose))
         {
-            wl_set_window_close();
+            wl_exit();
         }
         // render_draw_rect_push(box->rect, (Render_Color){ 212, 190, 152, 255 });
         render_end();
